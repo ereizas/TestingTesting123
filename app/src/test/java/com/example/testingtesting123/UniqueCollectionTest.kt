31c97505890714e46ae70bf1442e596ee5a243ca
Eric Reizas <tuo17224@temple.edu>
2024-04-18 11:52:50 -0400

    Created test for getting a placed Item from the list

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -31,13 +31,20 @@ class UniqueCollectionTest {
     }
 
     @Test
+    fun getPlacedItemFromList(){
+        collection.addItem(Item("Item1"))
+        collection.addItem(Item("Item2"))
+        assert (collection.get(1).name=="Item2")
+    }
+
+    @Test
     fun clearAllItems() {
         collection.addItem(Item("item1"))
         collection.addItem(Item("Item2"))
 
         val originalSize = collection.size()
         collection.clear()
-        val newSize = collection.size();
+        val newSize = collection.size()
 
         assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
     }
